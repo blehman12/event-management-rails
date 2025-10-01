24df98d92543a512abe13c1aec5b8b5034532778
Bob Lehman <blehman12@comcast.net>
2025-09-30 19:50:30 -0700

    Add admin authorization to protect all admin routes

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,12 +1,12 @@
 class Admin::BaseController < ApplicationController
   before_action :authenticate_user!
-  # Add admin role checking if needed:
-  # before_action :ensure_admin
+  before_action :ensure_admin!
   
   private
   
-  # Uncomment if you have admin role checking:
-  # def ensure_admin
-  #   redirect_to root_path unless current_user&.admin?
-  # end
+  def ensure_admin!
+    unless current_user&.role == 'admin'
+      redirect_to root_path, alert: 'Access denied. Admin privileges required.'
+    end
+  end
 end
